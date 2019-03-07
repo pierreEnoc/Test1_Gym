f25087db9a62c208b10bb76a0aa509148a767975
pierreEnoc <pierre_enoc@yahoo.fr>
2019-03-07 16:28:34 -0300

    comit1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 ( function(){
     const search = document.getElementById("search");
     const profile = document.getElementById("profile");
+    const commits = document.getElementById("commits");
     const url ="https://api.github.com/users";
     const client_id = "e54c26adf70681f4e8e0";
     const client_secret ="7df70004baf71fe3e8d4d0b9fbcb1507e5e24762";
@@ -23,7 +24,7 @@
     const profile = await profileResponse.json();
     const repos = await reposResponse.json();
     
-       return {profile,repos,};
+       return {profile,repos,commits};
     }
     
     function showProfile(user){
@@ -34,7 +35,8 @@
         <ul class="List-group List-group-flush">
          <li class="List-grou-item">Repositorios: <span class="badge badge-success">${user.public_repos}</span></li>   
          <li class="List-grou-item">Seguidores:<span class="badge badge-primary">${user.followers}</span></li> 
-         <li class="List-grou-item">Seguindo<span class="badge badge-info">${user.following}</span></li> 
+         <li class="List-grou-item">Seguindo<span class="badge badge-info">${user.following}</span></li>
+         <li class="List-grou-item">commit<span class="badge badge-info">${user.committer}</span></li> 
         </ul>
         <div class="card-body">
          <a href="${user.html_url}" target="_blank" class="btn btn-warning btn-block"> ver perfil</a>
@@ -59,25 +61,33 @@
             <span class="badge badge-success">starts: ${repo.stargazers_count}</span>
             <span class="badge badge-primary">Watch: ${repo.watchers_count}</span>
             <span class="badge badge-warning">Forks: ${repo.forks_count}</span>   
+           
             </div>
             </div>
         </div>`;
     });
 
-    function showCommits (commit){
+    function showCommits (commits){
         let output="";
         
-        commits.forEach(commit => {
-            output += `<div class="card card-body-mb-2">
-                <div class="row">
-                <div class="col-md-6"><a href="${commit.html_url}" target="_black">${commit.name}</a></div>
-                <div class="col-md-6">
-                <span class="badge badge-success">starts: ${commit.committer}</span>
-                        
-                </div>
-                </div>
-            </div>`;
-        });
+        commits.innerHTML =`<div class="row mt-3"> 
+        <div class="col-md-4">
+        <div class="card" style="width:18rem;">
+        <img class="card-img.top" src="${user.gists_url}" />
+        <ul class="List-group List-group-flush">
+         <li class="List-grou-item">Repositorios: <span class="badge badge-success">${user.public_commits}</span></li>   
+      
+        </ul>
+        <div class="card-body">
+         <a href="${commits.html_url}" target="_blank" class="btn btn-warning btn-block"> ver perfil</a>
+     </div>
+    </div>
+    </div>
+    <div class="col-md-8">
+    <div id="commits"></div>
+    </div>
+    </div>`;
+    
     }
     document.getElementById("repos").innerHTML = output;
     document.getElementById("commits").innerHTML = output;
